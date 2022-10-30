d9270c4d485c4248b5513cf3ea8d7d0f979eac9a
Antonio Rodrigo <93305169+rodrigozero@users.noreply.github.com>
2022-10-30 16:18:18 -0300

    Add files via upload

---
+++ InterfaceServidorBD.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.rmi.*;
+import java.util.Vector;
+/**
+ *
+ * @author rodri
+ */
+
+public interface InterfaceServidorBD extends Remote{
+    public int insere(String marca, String modelo, String memoria, String cor) throws RemoteException;
+    public Smartphone selecciona(int id) throws RemoteException;
+    public Vector lista() throws RemoteException;
+    public int apaga(int id) throws RemoteException;
+}
