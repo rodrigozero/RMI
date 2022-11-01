84285ad93da12391275650e87009a1ee4b52acbd
Antonio Rodrigo <lopesrodrigo184@gmail.com>
2022-10-31 22:40:58 -0300

    Criação dos métodos restantes

---
+++ src/SeleccionaTuplo.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.rmi.*;
+/**
+ *
+ * @author rodri
+ */
+
+public class SeleccionaTuplo{
+    public SeleccionaTuplo()
+    {
+        System.out.println("Arrancando o Cliente...");
+        // Vamos tentar aceder ao Servidor de Registos para recolher a interface
+        try
+        {
+            bd = (InterfaceServidorBD) Naming.lookup("rmi://127.0.0.1/ServidorBD_1");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Falhou o arranque do Cliente.\n"+e);
+	    System.out.println("Certifique-se que tanto o Servidor de Registos como a Aplicação Servidora estão a correr correctamente.\n");
+            System.exit(0);
+        }
+    }    
+    public void selecciona (String[] argv)
+    {
+        try
+        {
+            int id = Integer.parseInt(argv[0]);
+            System.out.println(bd.selecciona(id).desc());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Excepção durante chamada remotas:" +e);
+            System.exit(0);
+        }
+    }  
+ 
+    public static void main (String[] argv)
+    {
+        if (argv.length!=1)
+        {
+            System.out.println("Sintaxe:\n\tjava SeleccionaTuplo \"id\"");
+            System.exit(0);
+        }
+        SeleccionaTuplo i = new SeleccionaTuplo();
+        i.selecciona(argv);
+    }
+
+    private InterfaceServidorBD bd; // A interface para o objecto remoto
+}
